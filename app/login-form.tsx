b4f58c606f12911a6901ec6b7ca415596e153723
Karthi-47 <karthippm10@gmail.com>
2025-02-07 00:33:16 +0530

    Refactor login form and update authorization logic to redirect users to the dashboard instead of the home page

--- app/login-form.tsx
+++ app/login-form.tsx
@@ -11,6 +11,7 @@ import { Button } from '@/app/button';
 import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
+import { useFormState, useFormStatus } from 'react-dom';
  
 export default function LoginForm() {
   const searchParams = useSearchParams();
@@ -87,3 +88,4 @@ export default function LoginForm() {
     </form>
   );
 }
+
